f3661edc791f52e49fce8613e5705b5b19bb4c40
Sneha-289 <101625828+Sneha-289@users.noreply.github.com>
2022-05-12 19:01:56 +0530

    Add files via upload

---
+++ 3_Implementation/wiper.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int motor,wipersignal,LED;
+    char LED1,LED2,LED3,RED,BLUE,ORANGE;
+    {
+    printf(" LED state: ");
+    scanf("%d",&LED);
+    if(LED1==ON)
+    {while(1){
+        printf("wiper state: ");
+        scanf("%d",&wipersignal);
+        if(wipersignal==2)
+        {
+            printf("pot: ");
+            scanf("%d",&pot);
+            if(LED>2&&LED<8)
+            {
+                printf("wiper-speed = 25% \n");
+            }
+            else if(LED&&2)
+            {
+                printf("wiper-speed = 50% \n");
+            }
+            else if(LEDt>=2)
+            {
+                printf("wiper-speed = 75% \n");
+            }
+            else
+            {
+                printf("no wiper movement\n");
+            }
+        }
+        else
+        {
+            printf("switch on the wiper signal\n");
+        }
+        }
+    }
+        else
+        {
+            printf("switch on the motor\n");
+        }
+    }
+    }
